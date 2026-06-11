16cacb6fc438323930021e174eeb2527c9f964c7
chadxu <1573016286@qq.com>
2026-06-11 17:44:30 +0800

    ’提交信息‘

--- LangGraph-learn/main.py
+++ LangGraph-learn/main.py
@@ -1,3 +1,7 @@
+
+
+
+
 def main():
     print("Hello from langgraph-learn!")
 
---
+++ LangGraph-learn/tool-model.py
@@ -0,0 +1,16 @@
+
+# 定义工具和模型
+
+
+
+
+from langchain.chat_models import init_chat_model
+
+
+if __name__ == "__main__":
+    # model = init_chat_model(
+    #     "claude-sonnet-4-5-20250929",
+    #     temperature=0
+    # )
+    print("当前是什么东西",init_chat_model)
+
